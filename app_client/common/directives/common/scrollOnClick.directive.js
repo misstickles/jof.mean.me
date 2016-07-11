c40e72b911261933c8e4fec5a86dda1d3a9fd23e
misstickles <jofaircloth@gmail.com>
2016-07-11 20:50:32 +0100

    Created new directive for page header scroll.  Moved factories to factories folder.

---
+++ app_client/common/directives/common/scrollOnClick.directive.js
@@ -0,0 +1,24 @@
+(function() {
+	angular
+		.module('jofApp')
+		.directive('scrollOnClick', scrollOnClick);
+
+	function scrollOnClick() {
+		return {
+			restrict: 'A',
+			scope: {
+				scrollTo: '@'
+			},
+			link: function($scope, $element, $attrs) {
+				$element.on('click', function() {
+				    $('html, body').stop().animate({
+				        scrollTop: ($($scope.scrollTo).offset().top - 50)
+				    }, 1250, 'easeInOutExpo');
+				    event.preventDefault();
+
+//					$('html, body').animate({ scrollTop: $($scope.scrollTo).offset().top }, "easeInOutExpo");
+				});
+			}
+		};
+	}
+})();
